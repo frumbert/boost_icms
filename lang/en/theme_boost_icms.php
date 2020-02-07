674c06ad8cfa91e89a63344f2d549f60a63a8242
Tim St.Clair <tim.stclair@gmail.com>
2020-02-07 12:50:15 +1100

    initial commit

---
+++ lang/en/theme_boost_icms.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Boost (ICMS) child theme.
+ *
+ * @package    theme_boost_icms (child)
+ * @copyright  2020 tim st.clair
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'ICMS (Boost)';
+$string['choosereadme'] = 'Theme ICMS is a child theme of Boost. It adds block positions in the center column.';
+
+
+$string['region-footer-left'] = 'Footer (left)';
+$string['region-footer-middle'] = 'Footer (middle)';
+$string['region-footer-right'] = 'Footer (right)';
+
+$string['region-side-pre'] = 'Right';
+$string['region-middle-top'] = 'Middle-top';
+$string['region-middle-bottom'] = 'Middle-bottom';
+
+$string['privacy:metadata'] = 'The Boost Campus Child theme does not store any personal data about any user.';
